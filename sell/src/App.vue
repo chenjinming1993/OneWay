0b8ce741dc0b52ae31204783e35b81952309dc89
chenjinming1993 <136639250@qq.com>
2018-09-25 17:36:56 +0800

    #5-3  报错报错= =

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-    
+    <router-view/>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
 <script>
-
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
--- sell/src/router/index.js
+++ sell/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import header from '@/components/header/header'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/'
+      name: 'header.vue',
+      component: header
     }
   ]
 })
