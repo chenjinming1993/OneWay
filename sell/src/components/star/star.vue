62c43cd41ee30435dd3889ebd4fa40ae79278db2
chenjinming1993 <136639250@qq.com>
2018-10-15 17:33:42 +0800

    #6-12 half

--- sell/src/components/star/star.vue
+++ sell/src/components/star/star.vue
@@ -5,8 +5,73 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+  const LENGTH = 5;
+  const CLS_ON = 'on';
+  const CLS_HALF = 'half';
+  const CLS_OFF = 'off';
 
+  export default {
+    props: {
+      size: {
+        type: Number
+      },
+      score: {
+        type: Number
+      }
+    },
+    computed: {
+      starType() {
+        return 'star-' + this.size
+      }
+    }
+  }
 </script>
 <style lang='stylus' scoped>
+  @import "../../common/stylus/mixin.styl"
+  .star
+    font-size: 0
+    .star-item
+      display: inline-block
+      background-repeat: no-repeat
+    &.star-48
+      width: 20px
+      height: 20px
+      background-size 20px 20px
+      margin-right 22px
+      &:last-child
+        margin-right 0
+      &.on
+        bg-image('star48_on')
+      &.half
+        bg-image('star48_half')
+      &.off
+        bg-image('star48_off')
+    &.star-36
+      width: 15px
+      height: 15px
+      background-size 15px 15px
+      margin-right 6px
+      &:last-child
+        margin-right 0
+      &.on
+        bg-image('star36_on')
+      &.half
+        bg-image('star36_half')
+      &.off
+        bg-image('star36_off')
+    &.star-24
+      width: 10px
+      height: 10px
+      background-size 10px 10px
+      margin-right 3px
+      &:last-child
+        margin-right 0
+      &.on
+        bg-image('star24_on')
+      &.half
+        bg-image('star24_half')
+      &.off
+        bg-image('star24_off')
+    
 </style>
