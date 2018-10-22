53b9c40de49877555605870ee15609f40bfa847b
chenjinming1993 <136639250@qq.com>
2018-10-22 17:41:53 +0800

    # 7-3(1) food-list

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -10,7 +10,32 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="(item,index) in goods" :key="index" class="food-list">
+          <h1>{{item.name}}</h1>
+          <ul>
+            <li v-for="(food,index) in item.foods" :key="index" class="food-item">
+              <div class="icon">
+                <img :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+              </div>
+              <div class="extra">
+                <span>月售{{food.sellCount}}份</span>
+                <span>好评率{{food.rating}}%</span>
+              </div>
+              <div class="price">
+                <span>￥{{food.price}}</span>
+                <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
