b45ab8a081bef4b287999b723baaab7e8407a775
chenjinming1993 <136639250@qq.com>
2018-11-20 17:30:44 +0800

    # 8-4 接下来需要引入滚动

--- sell/src/components/food/foodDetail.vue
+++ sell/src/components/food/foodDetail.vue
@@ -2,10 +2,23 @@
 <template>
   <transition name="fadeFoodDetail">
     <div class="foodDetail" v-show="showFlag">
-      <div class="image-header">
-        <img :src="food.image">
-        <div class="back" @click="showFlag=false">
-          <i class="icon-arrow_lift"></i>
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image">
+          <div class="back" @click="showFlag=false">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+        <div class="content_fd">
+          <h2 class="name_fd">{{food.name}}</h2>
+          <div class="extra_fd">
+            <span style="margin-right:12px">月售{{food.sellCount}}份</span>
+            <span>好评率{{food.rating}}%</span>
+          </div>
+          <div class="price_fd">
+            <span class="now_fd">￥{{food.price}}</span>
+            <span v-show="food.oldPrice" class="old_fd">￥{{food.oldPrice}}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -46,28 +59,56 @@ export default {
     height 100%
     z-index 40
     background #fff
-  .image-header
-    position relative
-    width 100%
-    height auto // 从 0 改为 auto，避免图片拉伸
-    padding-top 100%
-    img
-      position absolute
-      top 0
-      left 0
-      width 100%
-      height auto
-    .back
-      position absolute
-      top 10px
-      left 5px
-      height: 24px
-      line-height: 24px
-      border-radius: 7px
-      background rgba(20,29,39,0.3)
-      text-align: center
-      .icon-arrow_lift
-        padding 10px
-        font-size: 16px
-        vertical-align top
+    .food-content
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+      .image-header
+        position relative
+        width 100%
+        height auto // 从 0 改为 auto，避免图片拉伸
+        padding-top 100%
+        img
+          position absolute
+          top 0
+          left 0
+          width 100%
+          height auto
+        .back
+          position absolute
+          top 10px
+          left 5px
+          height: 24px
+          line-height: 24px
+          border-radius: 7px
+          background rgba(143, 165, 189, 0.3)
+          text-align: center
+          .icon-arrow_lift
+            // display block
+            padding 10px
+            font-size: 16px
+            line-height 24px
+            vertical-align top
+            color white
+      .content_fd
+        margin 18px
+        .name_fd
+          font-size 14px
+          font-weight 700
+          color rgb(7,17,27)
+          line-height 14px
+        .extra_fd
+          font-size 10px
+          font-weight 700
+          margin 8px 0 18px 0
+          color #93999f
+          line-height 14px
+        .price_fd
+          font-weight 700
+          line-height 24px
+          .now_fd
+            font-size 14px
+            color rgb(240,20,20)
+          .old_fd
+            font-size 10px
+            color rgb(147,153,159)
+            text-decoration line-through
 </style>
