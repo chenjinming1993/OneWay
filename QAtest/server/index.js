5c247b28ab38f817ea133331a703dc497e7ed6c7
chenjinming1993 <136639250@qq.com>
2018-12-17 15:28:55 +0800

    # 获取数据成功

--- QAtest/server/index.js
+++ QAtest/server/index.js
@@ -10,8 +10,15 @@ const app = express()
 app.use(express.static('../dist'))
 var server = require('http').createServer(app)
 
-app.get('/api/name', function (req, res) {
-  conn.query('select name from architecture', function (error, results, fields) {
+// app.get('/api/name', function (req, res) {
+//   conn.query('select name from architecture', function (error, results, fields) {
+//     if (error) throw error
+//     res.json(results)
+//   })
+// })
+
+app.get('/api/list', function (req, res) {
+  conn.query('select * from architecture', function (error, results, fields) {
     if (error) throw error
     res.json(results)
   })
--- QAtest/src/components/architecture/architecture.vue
+++ QAtest/src/components/architecture/architecture.vue
@@ -4,6 +4,16 @@
     <ul>
       <li v-for="(user,index) in users" :key="index">
         {{user.name}}
+        {{user.id}}
+        {{user.area}}
+        {{user.level}}
+        {{user.cost_stone}}
+        {{user.cost_metal}}
+        {{user.cost_water}}
+        {{user.cost_plastic}}
+        {{user.cost_nuclear}}
+        {{user.cost_tech}}
+        {{user.cost_time}}
       </li>
     </ul>
   </div>
@@ -18,10 +28,20 @@ export default {
   },
   created() {
     var _this = this
-    this.$axios.get('/api/name', {}).then(function (response) {
+    this.$axios.get('/api/list', {}).then(function (response) {
       response.data.forEach(function(item, index) {
         _this.users.push({
-          'name': item.name
+          'name': item.name,
+          'id': item.id,
+          'area': item.area,
+          'level': item.level,
+          'cost_stone': item.cost_stone,
+          'cost_metal': item.cost_metal,
+          'cost_water': item.cost_water,
+          'cost_plastic': item.cost_plastic,
+          'cost_nuclear': item.cost_nuclear,
+          'cost_tech': item.cost_tech,
+          'cost_time': item.cost_time
         })
       })
     }).catch(function (error) {
