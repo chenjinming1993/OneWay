e386706f0e43e8ac1371b5a1f75bd2466e2b5f31
chenjinming1993 <136639250@qq.com>
2019-04-02 00:19:51 +0800

    #overview-right

--- sell/src/components/ratings/ratings.vue
+++ sell/src/components/ratings/ratings.vue
@@ -8,13 +8,29 @@
           <div class="title">综合评分</div>
           <div class="rank">高于周边商家{{seller.rankRate}}</div>
         </div>
-        <div class="overview-right"></div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import star from '../../components/star/star'
 export default {
   name: 'ratings',
   props: {
@@ -25,6 +41,9 @@ export default {
   data () {
     return {
     }
+  },
+  components: {
+    star
   }
 }
 
@@ -40,11 +59,16 @@ export default {
     .overview
       display flex
       padding 18px 0
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
       .overview-left
         flex 0 0 137px
+        width 137px
         border-right 1px solid rgba(7, 17, 27, 0.1)
         text-align center
         padding-bottom 6px
+        @media only screen and (max-width 320px)
+          flex 0 0 110px
+          width 110px
         .score
           font-size 24px
           line-height 28px
@@ -62,4 +86,36 @@ export default {
       .overview-right
         flex 1
         padding-left 24px
+        @media only screen and (max-width 320px)
+          padding-left 6px
+        .score-wrapper
+          margin-bottom 8px
+          .title
+            display inline-block
+            font-size 12px
+            color rgb(7,17,27)
+            vertical-align top
+          .star
+            display inline-block
+            margin 0 12px
+            line-height 18px
+            vertical-align top
+          .score
+            display inline-block
+            font-size 12
+            color rgb(255,153,0)
+            vertical-align top
+        .delivery-wrapper
+          .title
+            display inline-block
+            font-size 12px
+            color rgb(7,17,27)
+            margin-right 12px
+            vertical-align top
+          .delivery
+            display inline-block
+            font-size 13px
+            line-height 18px
+            color rgb(147,153,159)
+            vertical-align top
 </style>
