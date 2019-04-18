6aa8303710902ec64ae7917cce54e4205b1ed902
chenjinming1993 <136639250@qq.com>
2019-04-19 00:08:18 +0800

    #start seller

--- sell/src/components/seller/seller.vue
+++ sell/src/components/seller/seller.vue
@@ -1,13 +1,31 @@
 <!--  -->
 <template>
-  <div>I am seller</div>
+  <div class="seller">
+    <div class="overview">
+      <h1 class="title">{{seller.name}}</h1>
+      <div class="desc">
+        <star :size="36" :score="seller.score"></star>
+        <span class="text">({{seller.ratingCount}})</span>
+        <span class="text">月售{{seller.sellCount}}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import star from '../../components/star/star'
 export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
   data () {
     return {
     }
+  },
+  components: {
+    star
   }
 }
 
